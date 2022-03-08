18b99e96394c01c9657f1e133598dc5cd3645399
Matin Arshad <matinarshad001@gmail.com>
2022-03-08 23:39:59 +0000

    enabling the cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = require("./routes");
+const cors = require("cors");
 const startMetricServer = require("./monitor/metrics");
 
 const app = express();
 const port = 8080;
 app.use(express.json());
+app.use(cors());
 app.use("/", router);
 
 app.listen(port, () => {
