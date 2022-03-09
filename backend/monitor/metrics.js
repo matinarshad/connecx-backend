35e6d3e54906e0eb1fc5f5c9dee057a099a27689
Matin Arshad <matinarshad001@gmail.com>
2022-03-09 19:58:36 +0000

    added cors for metrcis endpoint as running it on a different port

--- backend/monitor/metrics.js
+++ backend/monitor/metrics.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const client = require("prom-client");
+const cors = require("cors");
 const app = express();
 const checkToken = require("../middleware/validateToken");
 
 module.exports = startMetricServer = () => {
   const collectDefaultMetrics = client.collectDefaultMetrics;
   collectDefaultMetrics();
-
+  app.use(cors());
   app.post("/metrics", checkToken, async (req, res) => {
     res.set("Content-Type", client.register.contentType);
     return res.send(await client.register.metrics());
